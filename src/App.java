b98e005557c5bc95cb3c05ad1630bbed94ddfbf9
alinetenorio <aline.tenorio96@gmail.com>
2022-02-24 23:53:01 -0300

    adicionando comparator

--- src/App.java
+++ src/App.java
@@ -1,101 +1,98 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class App {
 	static int[][] tab;
     
 	public static void main(String[] args) {
-			//escreva aqui o seu código
-			Scanner sc = new Scanner(System.in);
-		//	int n = sc.nextInt();
-		//	int q = sc.nextInt();
+		//escreva aqui o seu código
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int q = sc.nextInt();
 
-			int n = 5;
-			int q = 2;
-			
-			tab = new int[n][n];
-			
-			int op, x, r;
-			for(int i = 0; i<q; i++) {
-				op = sc.nextInt();
-				x = sc.nextInt();
-				
-				if(op < 3) {
+		int[][] tab = new int[n][n];
+		int op, x, r;
+		for(int i=0; i < q; i++) {
+			op = sc.nextInt();
+			x = sc.nextInt();
+
+			switch(op) {
+
+				case 1:
 					r = sc.nextInt();
-					
-					if( op == 1 ) opLinha(x, r);
-					else opColuna(x, r);
-					
-				} else {
-					if( op == 3 ) System.out.println(freqLinha(x));
-					else  System.out.println(freqColuna(x));
-				}
+					linha(tab, x, r);
+					break;
+
+				case 2:
+					r = sc.nextInt();
+					coluna(tab, x, r);
+					break;
+				case 3:
+					int[] linha = tab[x-1];
+					// Arrays.sort(linha);
+					Map<Integer, Integer> count = new HashMap<>();
+					for(int j = 0; j < linha.length; j++) {
+						if(count.containsKey(linha[j])) {
+							count.put(linha[j], count.get(linha[j]) + 1);
+						} else{
+							count.put(linha[j], 1);
+						}
+					}
+			
+					Map.Entry<Integer, Integer> a = (Map.Entry<Integer, Integer>) count.entrySet()
+							.stream()
+							.sorted(new Comp())
+							.toArray()[count.size() - 1];
+
+					System.out.println("obj: " + a.getValue());
+							
+
+				case 4:
 			}
 			
-			sc.close();
+		}
+
+		sc.close();
+
 	}
-	
-	static void opLinha(int x, int r) {
+
+	static void linha(int[][] tab, int x, int r) {
 		for(int i = 0; i < tab.length; i++) {
 			tab[x-1][i] = r;
 		}
 	}
-	
-	static void opColuna(int x, int r) {
+
+	static void coluna(int[][] tab, int x, int r) {
 		for(int i = 0; i < tab.length; i++) {
 			tab[i][x-1] = r;
 		}
 	}
-	
-	 static int freqLinha(int x) {
-		Arrays.sort(tab[x-1]);
-		int count = 0, countMax = 0, numMax = 0;
-		for(int i = 1; i < tab.length; i++) {
-			int anterior = tab[x-1][i-1];
-			int atual = tab[x-1][i];
-		 
-			if( atual == anterior ){
-				count ++;
-			} else {
-				if( count >= countMax ) {
-					countMax = count;
-					numMax = atual;
-					count = 0;
-				}
-			}
-		}
-		return numMax;
-	}
-	
-	static int freqColuna(int x) {
-		int[] coluna = new int[x];
 
-		for(int i = 0; i < tab.length; i++) {
-			coluna[i] = tab[i][x-1];
-		}
-		
-		Arrays.sort(coluna);
-		
-		int count = 0, countMax = 0, numMax = 0;
-		for(int i = 1; i < tab.length; i++) {
-			int anterior = coluna[i-1];
-			int atual = coluna[i];
-		 
-			if( atual == anterior ){
-				count ++;
-			} else {
-				if( count >= countMax ) {
-					countMax = count;
-					numMax = atual;
-					count = 0;
-				}				
-			}
-		}
-		return numMax;
-	}
 }
 
+class Comp implements Comparator<Map.Entry<Integer, Integer>> {
+
+	@Override
+	public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+		int qtd = Integer.compare(o1.getValue(), o2.getValue());
+		if(qtd != 0) return qtd;
+		
+		int key = Integer.compare(o1.getKey(), o2.getKey());
+		return key;
+
+	}
+
+	
+
+}
 //2 4
 
 // 1 1 1
